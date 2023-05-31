dabd897e8357b97e77370028ab36b23782bac17f
MRRazvi <mrrazvi@icloud.com>
2023-05-31 17:26:54 +0500

    wip

---
+++ woocommerce-hide-products.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * Plugin Name:       WooCommerce Hide Products
+ * Description:       Simple plugin to hide products on the basis of geolocation or popup country selection.
+ * Version:           1.0.0
+ * Author:            Zen Agency
+ * Author URI:        https://zen.agency
+ * Text Domain:       whp
+ */
