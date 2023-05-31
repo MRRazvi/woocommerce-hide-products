6c2c7377ad2da27a188f63c91983ce6a850813fc
MRRazvi <mrrazvi@icloud.com>
2023-05-31 17:37:37 +0500

    wip

--- woocommerce-hide-products.php
+++ woocommerce-hide-products.php
@@ -8,3 +8,23 @@
  * Author URI:        https://zen.agency
  * Text Domain:       whp
  */
+
+// register options page
+if (function_exists('acf_add_options_page')) {
+    acf_add_options_page(
+        array(
+            'page_title' => 'Akhurst Settings',
+            'menu_title' => 'Akhurst Settings',
+            'menu_slug' => 'akhurst-settings',
+            'capability' => 'edit_posts',
+            'redirect' => false
+        )
+    );
+}
+
+$whp_functionality_option = get_field('whp_functionality_option', 'option');
+if ($whp_functionality_option == '1') {
+
+} else if ($whp_functionality_option == '2') {
+    var_dump('22');
+}
