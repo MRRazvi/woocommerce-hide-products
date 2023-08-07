69a125b12ceef40a550197a4bf4df876e1abed4b
MRRazvi <mrrazvi@icloud.com>
2023-08-07 19:08:07 +0500

    wip

--- woocommerce-hide-products.php
+++ woocommerce-hide-products.php
@@ -10,19 +10,16 @@
  */
 
 define('WHP_USER_COUNTRY', 'whp_user_country');
+define('WHP_USER_LOCATION', 'user_location');
+define('WHP_POPUP_COOKIE', 'user_location_manual');
+define('WHP_GFORM_ID', '1');
 
+// optional acf options page
 if (function_exists('acf_add_options_page')) {
-    acf_add_options_page(
-        array(
-            'page_title' => 'Akhurst Settings',
-            'menu_title' => 'Akhurst Settings',
-            'menu_slug' => 'akhurst-settings',
-            'capability' => 'edit_posts',
-            'redirect' => false
-        )
-    );
+    acf_add_options_page();
 }
 
+// add custom field to product page
 add_action('woocommerce_product_options_general_product_data', 'whp_add_hide_in_usa_checkbox');
 function whp_add_hide_in_usa_checkbox()
 {
@@ -37,6 +34,7 @@ function whp_add_hide_in_usa_checkbox()
     );
 }
 
+// store custom field from store page
 add_action('woocommerce_process_product_meta', 'whp_save_hide_in_usa_checkbox');
 function whp_save_hide_in_usa_checkbox($product_id)
 {
@@ -44,7 +42,20 @@ function whp_save_hide_in_usa_checkbox($product_id)
     update_post_meta($product_id, '_hide_in_usa', $hide_in_usa);
 }
 
-function exclude_products_in_usa($query)
+// set cookie on form submission
+add_action('gform_after_submission', 'whp_form_submission', 10, 2);
+function whp_form_submission($entry, $form)
+{
+    if ($form['id'] == WHP_GFORM_ID) {
+        $country = $entry['1'];
+        setcookie(WHP_POPUP_COOKIE, $country, time() + (86400 * 30), '/');
+    }
+}
+
+// exclude products from the loop if hide_in_usa is checked
+// and user location is set to US
+add_action('pre_get_posts', 'whp_exclude_products_in_usa');
+function whp_exclude_products_in_usa($query)
 {
     if (is_admin() || current_user_can('administrator')) {
         return;
@@ -54,72 +65,57 @@ function exclude_products_in_usa($query)
         return;
     }
 
-    if (get_user_country_iso() == 'US') {
-        $query->set(
-            'meta_query',
-            array(
-                array(
-                    'key' => '_hide_in_usa',
-                    'value' => 'yes',
-                    'compare' => '!='
-                )
-            )
-        );
-    }
-}
+    $location = ''; // default location
 
-add_action('pre_get_posts', 'exclude_products_in_usa');
+    $method = get_field('whp_functionality_option', 'option');
 
-function get_user_country_iso()
-{
-    $api_key = 'AIzaSyD1YRYXah31kLjisAR_8ydpSfCuh_JleHQ';
-
-    $url = "https://www.googleapis.com/geolocation/v1/geolocate?key=$api_key";
-    $args = array(
-        'method' => 'POST',
-        'timeout' => 15,
-        'sslverify' => false,
-        'body' => json_encode(array('considerIp' => 'true')),
-        'headers' => array(
-            'Content-Type' => 'application/json',
-        ),
-    );
-
-    $response = wp_remote_post($url, $args);
-
-    if (!is_wp_error($response) && wp_remote_retrieve_response_code($response) === 200) {
-        $body = wp_remote_retrieve_body($response);
-        $data = json_decode($body, true);
-
-        if (isset($data['location']['lat']) && isset($data['location']['lng'])) {
-            $lat = $data['location']['lat'];
-            $lng = $data['location']['lng'];
-
-            $url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$api_key";
-            $response = wp_remote_get($url);
-
-            if (!is_wp_error($response) && wp_remote_retrieve_response_code($response) === 200) {
-                $body = wp_remote_retrieve_body($response);
-                $data = json_decode($body, true);
-
-                if (isset($data['results'][0]['address_components'])) {
-                    foreach ($data['results'][0]['address_components'] as $component) {
-                        if (in_array('country', $component['types'])) {
-                            return $component['short_name']; // ISO country code
-                        }
-                    }
-                }
-            }
+    if ($method == 1) {
+        if (isset($_COOKIE[WHP_USER_LOCATION])) {
+            $location = $_COOKIE[WHP_USER_LOCATION];
+        }
+    } else {
+        if (isset($_COOKIE[WHP_POPUP_COOKIE])) {
+            $location = $_COOKIE[WHP_POPUP_COOKIE];
         }
     }
 
-    return false;
+    if ($location == 'US' || $location == 'United States') {
+        $meta_query_args = array(
+            'relation' => 'OR',
+            array(
+                'key' => '_hide_in_usa',
+                'value' => 'yes',
+                'compare' => '!='
+            ),
+            array(
+                'key' => '_hide_in_usa',
+                'compare' => 'NOT EXISTS'
+            )
+        );
+
+        $query->set('meta_query', $meta_query_args);
+    }
 }
 
-function dd($data)
-{
-    echo '<pre>';
-    var_dump($data);
-    echo '<pre>';
-    die;
+// utility to hide popup on method one
+add_action('init', function () {
+    $method = get_field('whp_functionality_option', 'option');
+    if ($method == 1) {
+        setcookie(WHP_POPUP_COOKIE, 'none', time() + (86400 * 30), '/');
+    } else {
+        if (isset($_COOKIE[WHP_POPUP_COOKIE]) && $_COOKIE[WHP_POPUP_COOKIE] == 'none') {
+            setcookie(WHP_POPUP_COOKIE, '', time() - 3600, '/');
+        }
+    }
+});
+
+// helper method
+if (!function_exists('dd')) {
+    function dd($data)
+    {
+        echo '<pre>';
+        var_dump($data);
+        echo '<pre>';
+        die;
+    }
 }
